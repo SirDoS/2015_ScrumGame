4dc57f07889b6fae16efae0ccfe625f295c0f7c4
praxd <gabrielpraxedes@live.com>
2015-08-11 09:40:23 -0300

    First Commit

---
+++ Source/Assets/Scripts/BaseChar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseChar : MonoBehaviour {
+	private int vida;
+
+	public int Vida {
+		get {
+			return vida;
+		}
+		set {
+			vida = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
