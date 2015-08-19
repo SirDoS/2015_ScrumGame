8aa3939627185f138f314e2ed8919fc7451b17b5
Vanzz <tiivansan@hotmail.com>
2015-08-19 09:23:25 -0300

    Implementação do Panda, DENOVO
    Ve se da sync demônio do capeta

---
+++ Source/Assets/Scripts/Scripts/Character/Player_Panda/States/Panda_JumpState.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31.StateKit;
+
+public class Panda_JumpState : SKMecanimState<PandaController> {
+
+	//public bool jumped = false;
+	public override void begin ()
+	{
+		base.begin ();
+		
+		Vector2 currentVelocity = _context.physicsController.GetVelocity();
+		_context.physicsController.SetVelocity(new Vector2(currentVelocity.x,
+		                                                   _context.pandaJumpForce));
+		//Tocar animacao JUMP.
+		
+	}
+	
+	public override void reason(){
+		base.reason ();
+		
+		/*float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal");*/
+		
+		//Independente de qualquer condicao, passara para OnAirState
+		_machine.changeState<Panda_OnAirState>();
+		
+		
+	}
+	
+	#region implemented abstract members of SKMecanimState
+	public override void update (float deltaTime, AnimatorStateInfo stateInfo)
+	{
+	}
+	#endregion
+	
+	public override void end ()
+	{
+		base.end ();
+	}
+}
---
+++ Source/Assets/Scripts/Scripts/Character/Player_Panda/States/Panda_LandState.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31.StateKit;
+
+public class Panda_LandState : SKMecanimState<PandaController> {
+	public override void begin ()
+	{
+		base.begin ();
+		
+		//Tocar animacao LAND.
+		//Parar o personagem.
+	}
+	
+	public override void reason ()
+	{
+		base.reason ();
+		
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		
+		if(vertical == 0.0f && horizontal == 0.0f){
+			_machine.changeState<Panda_IdleState>();
+			return;
+		}
+		if(vertical == 0.0f && horizontal != 0){
+			_machine.changeState<Panda_RunState>();
+			return;
+		}
+		 if(vertical > 0){
+			_machine.changeState<Panda_JumpState>();
+			return;
+		}
+		
+	}
+	
+	
+	
+	#region implemented abstract members of SKMecanimState
+	public override void update (float deltaTime, AnimatorStateInfo stateInfo)
+	{
+		
+	}
+	#endregion
+	
+	public override void end ()
+	{
+		base.end ();
+	}
+}
