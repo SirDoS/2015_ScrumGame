5d40cec9409afc51c5e15c2b0994a866d1d6aeed
Vanzz <tiivansan@hotmail.com>
2015-08-28 10:24:26 -0300

    FALTOU O SWORD!

---
+++ Source/Assets/Scripts/Weapon/SwordScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordScript : BaseWeapon {
+
+	public override void Attack ()
+	{
+		base.Attack ();
+
+		foreach(RaycastHit2D hit in GetEnemiesAhead()){
+			Debug.Log(hit.transform.name);
+			BaseActor actor = hit.transform.GetComponent<BaseActor>();
+			DoDamage(actor);
+		}
+	}
+
+	public override void DoDamage (BaseActor pTarget)
+	{
+		Debug.Log("Sword Do Damage");
+
+		base.DoDamage (pTarget);
+	}
+}
