95f9757a7f07b957d8413c809d9e3ec885b97dc1
praxd <gabrielpraxedes@live.com>
2015-09-21 11:29:48 -0300

    É o que clan, condição de termino da fase, fase com inicio e fim Signed-off-by: praxd <gabrielpraxedes@live.com>

---
+++ Source/Assets/Scripts/Controllers/VictoryController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryController : MonoBehaviour {
+
+	bool isPlayerInside = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isPlayerInside){
+			Debug.Log("GANHOU FILHO DA PUTA!!!");
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D ganhador){
+		GirlController girlController = ganhador.gameObject.GetComponent<GirlController>();
+		if(girlController != null){
+			isPlayerInside = true;
+		}
+	}
+}
