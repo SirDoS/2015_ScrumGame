d9e516bbb9663f3b0b1b1a2422bfaec2db530e5d
praxd <gabrielpraxedes@live.com>
2015-09-29 09:06:27 -0300

    Fase Demo alterada, e câmera
    Signed-off-by: praxd <gabrielpraxedes@live.com>

--- Source/Assets/Scripts/Camera/CameraTest.cs
+++ Source/Assets/Scripts/Camera/CameraTest.cs
@@ -8,8 +8,10 @@ public class CameraTest : MonoBehaviour {
 	public Vector3 cameraOffset;
 
 	private void LateUpdate(){
+
 		transform.position = Vector3.MoveTowards(transform.position, 
-		                                         target.position + cameraOffset, 
+		                                         target.position + cameraOffset + new Vector3(3, 0,0), 
 		                                         Time.deltaTime * cameraSpeed);
+
 	}
 }
