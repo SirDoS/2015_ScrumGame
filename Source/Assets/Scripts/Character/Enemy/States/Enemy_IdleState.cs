7c4cb4cfad6439dde3ea0c9d6df50be6a79d2fd0
Vanzz <tiivansan@hotmail.com>
2015-10-21 10:39:26 -0200

    Vai curintia

--- Source/Assets/Scripts/Character/Enemy/States/Enemy_IdleState.cs
+++ Source/Assets/Scripts/Character/Enemy/States/Enemy_IdleState.cs
@@ -6,12 +6,15 @@ public class Enemy_IdleState : SKMecanimState<EnemyController> {
 
 	public override void begin()
 	{
-		bool landed;
+		bool landed = false;
 		base.begin ();
 
 		if(!landed){
 			landed = true;
 			_context.physicsController.SetVelocity(new Vector2(0.05f, 0.0f));
+		}
+		if(landed){
+			_context.physicsController.SetVelocity(Vector2.zero);
 		}
 		_context.animatorController.PlayState("Enemy1_Idle");
 	}
